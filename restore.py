a23c697e6fb1290e5b35a0fe9e727b7c9f490d40
vizier <vizier.bi@gmail.com>
2019-11-14 11:38:23 +0800

    添加创建文件函数

--- restore.py
+++ restore.py
@@ -1,6 +1,17 @@
 #!/bin/python3
 import os
+import datetime
 
+backup_path = '/tmp/mysql_back/'
+# 还原当前时间点备份文件
+t_time = datetime.date.today()
+
+
+def create_txt():
+    extend = 'ls ' + backup_path + str(t_time) + '> sqlfile.txt'
+	    extend1 = "cat sqlfile.txt | awk -F '.' '{print $1}' >sql_basename.txt"
+		    os.system(extend)
+			    os.system(extend1)
 def extends():
 	with open('sqlfile.txt', 'rb') as f:
 		sql1 = f.readlines()
@@ -11,4 +22,5 @@ def extends():
 			os.system('gunzip <' + sql_gzip_name + '| mysql -uroot -p12345678' + str(sql_base_name))
 
 if __name__ == '__main__':
+	create_txt()
 	extends()
