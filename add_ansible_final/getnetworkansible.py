0bb8728a1be0ee10d84a09d451c9d4f5f9bdf7c6
ene_project <ene_project@enesource.com>
2020-09-01 13:18:31 +0800

    全局扫描ip并免密登陆添加ansible主机

---
+++ add_ansible_final/getnetworkansible.py
@@ -0,0 +1,69 @@
+# @Time : 2020/8/31 
+# @Author : VizierBi
+import os
+import platform
+import socket
+import threading
+
+t = os.path.join(os.getcwd(), 't.txt')
+ip_sum = 0
+residue_ip = []
+
+
+def get_os():
+    os = platform.system()
+    if os == "Windows":
+        return "n"
+    else:
+        return "c"
+
+
+def ping_ip(ip_all):
+    l_suffix = []
+    cmd = ["ping", "-{op}".format(op=get_os()),
+           "1", ip_all]
+
+    output = os.popen(" ".join(cmd)).readlines()
+    # print(output)
+    for s in output:
+        if str(s).upper().find("TTL") >= 0:
+            # ip_suffix = ip_all.split(".")[-1]
+            l_suffix.append(ip_all)
+            # print(l_suffix)
+            # return l_suffix
+    return l_suffix
+
+
+def get_ip(ip_prefix):
+    l2 = []
+    global ip_sum
+    for i in range(1, 256):
+        ip = '%s.%s' % (ip_prefix, i)
+        myip2 = ping_ip(ip)
+        l2 += myip2
+        ip_sum += 1
+    print(ip_sum)
+    print(l2)
+    for ip_a in l2:
+        # os.system("ssh-copy-id -i ~/.ssh/id_rsa.pub root@{}".format(ip_a))
+        os.system("cat " + t + " | while read passwd;do sshpass -p $passwd ssh-copy-id " + ip_a + ";done")
+    return l2
+
+
+def get_local_ip():
+    myhosts = socket.getfqdn(socket.gethostname())
+    myaddr = socket.gethostbyname(myhosts)
+    return myaddr
+
+
+def add_ansible_host(myip_prefix):
+    ss = get_ip(myip_prefix)
+    for hostip in ss:
+        os.system("sed -i '28i 172.20.10.{}' /etc/ansible/hosts".format(hostip))
+
+
+if __name__ == "__main__":
+    myip = get_local_ip()
+    myip_prefix = '.'.join(myip.split('.')[:-1])
+    # get_ip(myip_prefix)
+    add_ansible_host(myip_prefix)
