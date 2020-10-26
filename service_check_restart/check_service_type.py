2884df3debfc7d81632ec22cc3e74ea9ee70dd6d
ene_project <ene_project@enesource.com>
2020-10-26 10:26:50 +0800

    针对混合服务进行改进

--- service_check_restart/check_service_type.py
+++ service_check_restart/check_service_type.py
@@ -21,42 +21,58 @@ class SerPorts(Enum):
 
 
 def check_service():
-    if os.system('echo ' + Hostname + '| grep mysql') == 0:
+    elif os.system('echo ' + Hostname + '| grep mysql') == 0:
         servicename = 'mysqld'
         vbi = CheckService(servicename, str(SerPorts.mysql.value))
         vbi.check_ports()
         vbi.checkprocess()
-    if os.system("echo " + Hostname + " |grep postgresql") == 0:
+    elif os.system("echo " + Hostname + " |grep postgresql") == 0:
         servicename = 'postgresql-11'
         vbi = CheckService(servicename, str(SerPorts.postgresql.value))
         vbi.check_ports()
         vbi.checkprocess()
-    if os.system("echo " + Hostname + " |grep rabbitmq") == 0:
+    elif os.system("echo " + Hostname + " |grep rabbitmq") == 0:
         servicename = 'rabbitmq'
         vbi = CheckService(servicename, str(SerPorts.rabbitmq.value))
         vbi.check_ports()
         vbi.checkprocess()
-    if os.system("echo " + Hostname + " |grep redis ") == 0:
+    elif os.system("echo " + Hostname + " |grep redis ") == 0:
         servicename = 'redis'
         vbi = CheckService(servicename, str(SerPorts.redis.value))
         vbi.checkprocess()
         vbi.check_ports()
-    if os.system("echo " + Hostname + " |grep mongodb") == 0:
+    elif os.system("echo " + Hostname + " |grep mongodb") == 0:
         servicename = " mongodb"
         vbi = CheckService(servicename, str(SerPorts.mongodb.value))
         vbi.check_ports()
         vbi.checkprocess()
-    if os.system("echo " + Hostname + " |grep backend") == 0:
+    elif os.system("echo " + Hostname + " |grep backend") == 0:
         servicename = "tomcat"
         vbi = CheckService(servicename, str(SerPorts.tomcat.value))
         vbi.check_ports()
         vbi.checkprocess()
-    if os.system("echo " + Hostname + " |grep front") == 0:
+    elif os.system("echo " + Hostname + " |grep front") == 0:
         servicename = "nginx"
         vbi = CheckService(servicename, str(SerPorts.nginx.value))
         vbi.check_ports()
         vbi.checkprocess()
-
+    else:
+	autore_log.Info("此服务器运行的为混合服务，将进行检测有哪些服务在运行...")
+	host_sets = {"k8s-master": {"mysql", "redis"},
+                     "k8s-node1": {"tomcat", "nginx"}
+                     }
+	if Hostname in host_sets.keys():
+            auto("当前主机{}存在的服务有{}".format(Hostname, host_sets[Hostname]))
+            for i in host_sets[Hostname]:
+                if os.system("ps -ef|grep " + i + '|grep -v grep') == 0:
+                    autore_log.Info("{}进程存在，服务正常".format(i))
+                else:
+                    autore_log.Error("{}进程消失，服务异常，正在重启服务".format(i))
+                    os.system("systemctl restart " + i)
+                    if os.system("ps -ef|grep " + i + '|grep -v grep') == 0:
+                        autore_log.Info("{}进程存在，重启服务成功".format(i))
+                    else:
+                        autore_log.Error("重启{}失败，请手动检测！！！！".format(i))
 
 class CheckService(object):
     def __init__(self, servicename, port):
