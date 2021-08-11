a3fa0fd8339a22d8463ce841e51b24b9027df625
vizier <vizier@vizierdeMacBook-Pro.local>
2021-08-11 11:02:38 +0800

    更新系统初始化脚本

--- inttools.sh
+++ inttools.sh
@@ -14,7 +14,41 @@ systemctl enable docker && systemctl start docker
 mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
 wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
 wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
-
+#同步时间
+yum install chrony net-tools wget vim -y
+systemctl enable chronyd && systemctl start chronyd
+cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime  -y
+#修改系统参数
+sed -i'' 's,SELINUX=enforcing,SELINUX=disabled,g' /etc/selinux/config
+setenforce 0
+systemctl stop libvirtd.service && systemctl disable libvirtd.service && systemctl status libvirtd.service
+sh -c 'echo " * hard nofile 65536 " >> /etc/security/limits.conf'
+sh -c 'echo " * soft nofile 65536 " >> /etc/security/limits.conf'
+sh -c 'echo " * soft nproc  65536 " >> /etc/security/limits.conf'
+sh -c 'echo " * hard nproc  65536 " >> /etc/security/limits.conf'
+sh -c 'echo " * soft nproc  1024000" >> /etc/security/limits.d/20-nproc.conf'
+sh -c 'echo " * hard nproc  1024000" >> /etc/security/limits.d/20-nproc.conf'
+#pip 加速 
+cat <<EOF> /etc/pip.conf
+[global]
+index-url = http://mirrors.aliyun.com/pypi/simple/
+[install]
+trusted-host=mirrors.aliyun.com
+EOF
+#docker加速
+mkdir -p /etc/docker/
+cd /etc/docker/
+cat <<EOF> /etc/docker/daemon.json
+{
+ "registry-mirrors": ["https://iby0an85.mirror.aliyuncs.com"]
+}
+EOF
+#安装vm需要的软件
+yum install -y acpid
+systemctl enable acpid.service &  systemctl start acpid.service 
+yum install -y epel-release.noarch
+yum install -y cloud-init
+yum install -y cloud-utils-growpart
 yum clean all
 yum makecache
 yum -y install centos-release-scl
